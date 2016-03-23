2c98141d09283928d907ab9b08f8055151abe36a
kalyan <kalyan@staging.eimpressive.com>
2016-03-23 12:40:45 +0530

    fourthforce_deletezoom_fourth 2

--- www/js/update_activitya.js
+++ www/js/update_activitya.js
@@ -524,8 +524,8 @@ var settings = {
     //alert('success');
     sessionStorage.setItem("ret",JSON.stringify(data));
     ret =  JSON.parse(sessionStorage.getItem("ret"));
-    //var res = ret.slice(12,100);
-    var res = ret.slice(137,300);
+    var res = ret.slice(12,100);  //live
+   // var res = ret.slice(137,300); //stage
    // alert(res+'res');
    //alert(verification_user_id+'verification_user_id');
       //alert(coordinator_id+'coordinator_id');
